94ce8e623ac411bc61fc5d9a7375466d36675b31
logan <musashimiyamoto@interia.eu>
2017-08-23 13:54:30 +0200

    nowe

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+﻿module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/main.css': 'sass/main.sass'
+				}
+			}
+		},
+
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+
+		browserSync: {
+			bsFiles: {
+				src: ['css/main.css', 'index.html']
+			},
+			options: {
+				watchTask: true,
+				server: {
+					baseDir: "./"
+				}
+			}
+		},
+
+		jshint: {
+			all: ['js/*.js']
+		},
+
+		watch: {
+			scripts: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			}
+		}
+
+
+	});
+
+	// Load the plugins tasks
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	// Default task(s).
+
+	grunt.registerTask('default', ['sass', 'imagemin', 'jshint', 'watch']);
+};
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+﻿// First comment
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+/* 
+    Second comment
+    comment
+*/
+console.log('Hello, ' + name);
